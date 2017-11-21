eccd06469ff074c827e8a41b1b458ddde7c0d7b6
root <root@localhost.localdomain>
2017-11-21 22:05:52 +0400

    Add some improvement to job log viewer python script

--- jenkins-python/app/getlog.py
+++ jenkins-python/app/getlog.py
@@ -9,17 +9,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-	str='You can search job log by id or datetime<br/><br/>If you want to search by id just type http://host:5000/jobid/(type here jobid)<br/><br/>Otherwise, type http://host:5000/jobdate/(type here jobid build time), date format YYYY-MM-DDTHH:MI<br/><br/>Thank you!'
+	str='You can search job log by id or datetime<br/><br/>If you want to search by id just type http://host:5000/yourprojectname/id/(type here jobid)<br/><br/>Otherwise, type http://host:5000/yourprojectname/date/jobdate/(type here jobid build time), date format YYYY-MM-DDTHH:MI<br/><br/>Thank you!'
 	return str
 
-@app.route('/jobid/<int:id>')
-def search_byid(id):
+@app.route('/<string:projectname>/id/<int:id>')
+def search_byid(projectname,id):
         es = Elasticsearch([{'host': 'jenkins-elasticsearch', 'port': 9200}])
 	a=es.search(index='logstash',body={
 					"query": {
 						"bool": {
 							"must": [
 								{ "match_phrase_prefix": { "data.buildNum":   id }},
+								{ "match_phrase_prefix": { "data.projectName":   projectname }}
 								],
 							}
 						}
@@ -27,14 +28,15 @@ def search_byid(id):
 #        b=json.dumps(a,sort_keys=True, indent=5)
         return jsonify(a)
 
-@app.route('/jobdate/<datetime>')
-def search_bydatetime(datetime):
+@app.route('/<string:projectname>/date/<datetime>')
+def search_bydatetime(projectname,datetime):
         es = Elasticsearch([{'host': 'jenkins-elasticsearch', 'port': 9200}])
         a=es.search(index='logstash',body={
                                         "query": {
                                                 "bool": {
                                                         "must": [
                                                                 { "match_phrase_prefix": { "@buildTimestamp":   datetime }},
+								{ "match_phrase_prefix": { "data.projectName":   projectname }}
                                                                 ],
                                                         }
                                                 }
